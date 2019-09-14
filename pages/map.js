705690c192c5725d71284b08cc887195251b3b06
Johnnie Hård <johnnie@synvinkel.org>
2019-09-14 18:32:27 +0200

    working dynamically imported map component

--- pages/map.js
+++ pages/map.js
@@ -1,41 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react'
-import "mapbox-gl/dist/mapbox-gl.css"
-import mapboxgl from 'mapbox-gl'
+import dynamic from 'next/dynamic'
 
-function Map() {
+const DynamicMap = dynamic(() => import('../components/Map'), {
+    loading: () => <p>Loading...</p>,
+    ssr: false
+})
 
-    const [map, setMap] = useState(null);
-
-    const ref = useRef(null);
-
-    // Initialize map
-    useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiaWFtbHVrZXNreSIsImEiOiJjamV0MW9rbTQwdjM2MnJvYnVmaTVwMTF4In0.865aEaV_zPu8st9-vXNeFg'
-        const map = new mapboxgl.Map({
-            container: ref.current,
-            style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-            center: [18, 59],
-            zoom: 10
-        })
-
-        map.on("load", () => {
-            setMap(map)
-        })
-
-    }, [])
-
-    return (
-        <>
-            <div className="Map" ref={el => (ref.current = el)} />
-            <style jsx>{`
-                .Map{
-                    height: 500px;
-                }
-                `}</style>
-                {/* <style jsx global>{mapboxcss}</style> */}
-        </>
-    )
-
+const Map = () => (
+    <div className="mapcontainer">
+        <DynamicMap />
+        <style jsx global>{`
+body{
+    margin: 0;
 }
+
+.mapcontainer {
+    height: 100vh;
+}
+        `}</style>
+    </div>
+)
 
 export default Map
