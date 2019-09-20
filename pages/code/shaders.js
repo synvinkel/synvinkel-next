0429b3f6097eb8994035341d381209f03ffaab0b
Johnnie Hård <johnnie@synvinkel.org>
2019-09-20 22:32:04 +0200

    shader fence

--- pages/code/shaders.js
+++ pages/code/shaders.js
@@ -151,6 +151,39 @@ const Shaders = () => {
                     <p>0006</p>
                 </div>
 
+                <div className="canvascontainer">
+                    <GLSLCanvas
+                        width={width} height={height}
+                        fragment={`
+                    #ifdef GL_ES
+                    precision mediump float;
+                    #endif
+                    
+                    uniform vec2 u_resolution;
+                    uniform vec2 u_mouse;
+                    uniform float u_time;
+
+                    float plot(vec2 st, float pct){
+                        return  smoothstep(pct-0.02, pct, st.y) -
+                                smoothstep(pct, pct+0.02, st.y);
+                    }
+                    void main() {
+                        vec2 st = gl_FragCoord.xy/u_resolution;
+
+                        float y = st.x;
+
+                        vec3 color = vec3(y);
+
+                        float pct = plot(st, y);
+                        color = (1.0-pct)*color+pct*vec3(0.0, 1.0, 0.0);
+                        
+                        gl_FragColor = vec4(color, 1.0);
+                    }        
+                    `}
+                    />
+                    <p>0007</p>
+                </div>
+
                 <style jsx global>{`
 body {
     padding: 0;
