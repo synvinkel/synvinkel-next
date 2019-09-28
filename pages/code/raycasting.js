35db85ed0b82e577d56f660c212a8049f534fb83
Johnnie Hård <johnnie@synvinkel.org>
2019-09-28 09:07:36 +0200

    shiffman raycasting lineline intersect

--- pages/code/raycasting.js
+++ pages/code/raycasting.js
@@ -34,7 +34,7 @@ class Ray {
     }
 
     show(ctx) {
-        const {x, y} = this.pos
+        const { x, y } = this.pos
         ctx.save()
         ctx.translate(x, y)
         ctx.fillStyle = 'red'
@@ -46,6 +46,27 @@ class Ray {
         ctx.lineTo(this.dir.x * 20, this.dir.y * 20)
         ctx.stroke()
         ctx.restore()
+    }
+
+    cast(wall) {
+        const { x: x1, y: y1 } = wall.a
+        const { x: x2, y: y2 } = wall.b
+        const { x: x3, y: y3 } = this.pos
+        const x4 = this.pos.x + this.dir.x
+        const y4 = this.pos.y + this.dir.y
+
+        const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+        if (den === 0) {
+            return
+        }
+        const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
+        const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den
+
+        if(t > 0 && t < 1 && u > 0) {
+            return true
+        } else {
+            return
+        }
     }
 }
 
@@ -79,8 +100,12 @@ class Raycasting extends Component {
         this.ctx.fillStyle = 'black'
         this.ctx.fillRect(0, 0, this.width, this.height)
         const { boundaries, rays } = this.state
+
+        rays.forEach(ray => boundaries.forEach(wall => ray.cast(wall)))
+
         boundaries.forEach(boundary => boundary.show(this.ctx))
         rays.forEach(ray => ray.show(this.ctx))
+
         requestAnimationFrame(this.loop)
     }
 
