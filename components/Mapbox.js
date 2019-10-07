838a4382ce4d3f903187b4c790a159428030cc5f
Johnnie Hård <johnnie@synvinkel.org>
2019-10-07 18:43:28 +0200

    Mapbox map with React context as per https://blog.dubenko.dev/life-after-google/

--- components/Mapbox.js
+++ components/Mapbox.js
@@ -1,18 +1,20 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useContext, createContext } from 'react'
 import "mapbox-gl/dist/mapbox-gl.css"
 import mapboxgl from 'mapbox-gl'
 
-function Map() {
+const MapContext = createContext(undefined)
+export const useMap = () => useContext(MapContext)
 
-    const [map, setMap] = useState(null);
+function Mapbox({ children }) {
 
-    const ref = useRef(null);
+    const mapRoot = useRef(undefined);
+    const [map, setMap] = useState(undefined);
 
     // Initialize map
     useEffect(() => {
         mapboxgl.accessToken = 'pk.eyJ1IjoiaWFtbHVrZXNreSIsImEiOiJjamV0MW9rbTQwdjM2MnJvYnVmaTVwMTF4In0.865aEaV_zPu8st9-vXNeFg'
         const map = new mapboxgl.Map({
-            container: ref.current,
+            container: mapRoot.current,
             style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
             center: [18, 59],
             zoom: 10
@@ -25,16 +27,23 @@ function Map() {
     }, [])
 
     return (
-        <>
-            <div className="Map" ref={el => (ref.current = el)} />
+        <div
+            className="Map"
+            ref={mapRoot}
+        >
+            {map !== undefined && (
+                <MapContext.Provider value={map}>
+                    {children}
+                </MapContext.Provider>
+            )}
             <style jsx>{`
                 .Map{
                     height: 100%;
                 }
                 `}</style>
-        </>
+        </div>
     )
 
 }
 
-export default Map
+export default Mapbox
--- pages/map.js
+++ pages/map.js
@@ -2,29 +2,31 @@ import dynamic from 'next/dynamic'
 import withLayout from '../components/Layout'
 import Header from '../components/Header'
 
-const DynamicMap = dynamic(() => import('../components/Map'), {
+const DynamicMap = dynamic(() => import('../components/Mapbox'), {
     loading: () => <p>Loading...</p>,
     ssr: false
 })
 
 const Map = () => (
     <>
-    <Header />
-    <main>
+        <Header />
+        <main>
 
-        <div className="mapcontainer">
-            <DynamicMap />
-            <style jsx global>{`
-body{
-    margin: 0;
-}
+            <div className="mapcontainer">
+                <DynamicMap></DynamicMap>
+                <style jsx global>{
+                    `
+    body{
+        margin: 0;
+    }
 
-.mapcontainer {
-    height: 70vh;
+    .mapcontainer {
+        height: 70vh;
 }
-        `}</style>
-        </div>
-    </main>
+`
+                }</style>
+            </div>
+        </main>
     </>
 )
 
